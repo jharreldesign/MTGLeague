bf974c32c53bd6b36e53aa4b87b7b5ec2a70f269
jharreldesign <harrel.jason@gmail.com>
2024-10-01 13:48:56 -0700

    Added a random function that allows card images to appear in the companion and ability screen.

--- JS/axios/cardimages.js
+++ JS/axios/cardimages.js
@@ -4,58 +4,9 @@
 //Each Planeswalker will choose a random out of the amount avaliable.
 //But beware, you need to make sure you choose different for each page so they're not the same on multiple pages
 
-//----------------COMPANIONS------------------------------------//
-// const blbCompanionOne = document.querySelector("#blbCompanionOne");
-// const blbCompanionTwo = document.querySelector("#blbCompanionTwo");
-// const blbCompanionThree = document.querySelector("#blbCompanionThree");
-
-// const dmuCompanionOne = document.querySelector("#dmuCompanionOne");
-// const dmuCompanionTwo = document.querySelector("#dmuCompanionTwo");
-// const dmuCompanionThree = document.querySelector("#dmuCompanionThree");
-
-// const dskCompanionOne = document.querySelector("#dskCompanionOne");
-// const dskCompanionTwo = document.querySelector("#dskCompanionTwo");
-// const dskCompanionThree = document.querySelector("#dskCompanionThree");
-
-// const mkmCompanionOne = document.querySelector("#mkmCompanionOne");
-// const mkmCompanionTwo = document.querySelector("#mkmCompanionTwo");
-// const mkmCompanionThree = document.querySelector("#mkmCompanionThree");
-
-// const otjCompanionOne = document.querySelector("#otjCompanionOne");
-// const otjCompanionTwo = document.querySelector("#otjCompanionTwo");
-// const otjCompanionThree = document.querySelector("#otjCompanionThree");
-
-//----------------LARGE IMAGE-----------------------------------//
-// const blbLargeImage = document.getElementById("blbLargeCompanion");
-// const dmuLargeImage = document.getElementById("dmuLargeCompanion");
-// const dskLargeImage = document.getElementById("dskLargeCompanion");
-// const mkmLargeImage = document.getElementById("mkmLargeCompanion");
-// const otjLargeImage = document.getElementById("otjLargeCompanion");
-
-//----------------ABILITIES-------------------------------------//
-// const blbAbilitiesOne = document.querySelector("#blbAbilitiesOne");
-// const blbAbilitiesTwo = document.querySelector("#blbAbilitiesTwo");
-// const blbAbilitiesThree = document.querySelector("#blbAbilitiesThree");
-
-// const dmuAbilitiesOne = document.querySelector("#dmuAbilitiesOne");
-// const dmuAbilitiesTwo = document.querySelector("#dmuAbilitiesTwo");
-// const dmuAbilitiesThree = document.querySelector("#dmuAbilitiesThree");
-
-// const dskAbilitiesOne = document.querySelector("#dksAbilitiesOne");
-// const dskAbilitiesSmallTwo = document.querySelector("#dksAbilitiesTwo");
-// const dskAbilitiesThree = document.querySelector("#dksAbilitiesThree");
-
-// const mkmAbilitiesOne = document.querySelector("#mkmAbilitiesOne");
-// const mkmAbilitiesTwo = document.querySelector("#mkmAbilitiesTwo");
-// const mkmAbilitiesThree = document.querySelector("#mkmAbilitiesThree");
-
-// const otjAbilitiesOne = document.querySelector("#otjAbilitiesOne");
-// const otjAbilitiesTwo = document.querySelector("#otjAbilitiesTwo");
-// const otjAbilitiesThree = document.querySelector("#otjAbilitiesThree");
-
 // ----------------------------------------COMPANIONS---------------------------------------------------//
 
-const firstCompanionBlb = async () => {
+const bloomburrowCompanion = async () => {
         let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Ablb`);
         let blbCompanions = response.data.data; 
         
@@ -70,8 +21,7 @@ const firstCompanionBlb = async () => {
         }
 };
 
-
-const secondCompanionDmu = async () => {
+const dominariaCompanion = async () => {
     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Admu`);
     let dmuCompanions = response.data.data; 
     
@@ -86,7 +36,7 @@ const secondCompanionDmu = async () => {
     }
 };
 
-const thirdCompanionDsk = async () => {
+const duskmournCompanion = async () => {
     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Adsk`);
     let dskCompanions = response.data.data; 
     
@@ -101,116 +51,205 @@ const thirdCompanionDsk = async () => {
     }
 };
 
-firstCompanionBlb();
-secondCompanionDmu();
-thirdCompanionDsk();
+const murdersCompanion = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Amkm`);
+    let mkmCompanions = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * mkmCompanions.length);
+    
+    let randomCompanion = mkmCompanions[randomIndex];
+    
+    if (randomCompanion.image_uris && randomCompanion.image_uris.art_crop) {
+        let imageUrl = randomCompanion.image_uris.art_crop;
+        
+        mkmCompanion.setAttribute("src", imageUrl);
+    }
+};
 
-// const firstCompanionBlb = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Ablb`);
-//     let blbFirstCompanion = response.data.data[10].image_uris.art_crop;
-//     let imageUrl = blbFirstCompanion
-//     blbCompanionOne.setAttribute("src", imageUrl); // Set the image src
-// };
+const outlawsCompanion = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Aotj`);
+    let otjCompanions = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * otjCompanions.length);
+    
+    let randomCompanion = otjCompanions[randomIndex];
+    
+    if (randomCompanion.image_uris && randomCompanion.image_uris.art_crop) {
+        let imageUrl = randomCompanion.image_uris.art_crop;
+        
+        otjCompanionOne.setAttribute("src", imageUrl);
+    }
+};
 
-// const secondCompanionBlb = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Ablb`);
-//     let blbSecondCompanion = response.data.data[91].image_uris.art_crop;
-//     let imageUrl = blbSecondCompanion
-//     blbCompanionTwo.setAttribute("src", imageUrl); // Set the image src
-// };
+// ----------------------------------------COMPANIONS---------------------------------------------------//
 
-// const thirdCompanionBlb = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Ablb`);
-//     let blbThirdCompanion = response.data.data[29].image_uris.art_crop;
-//     let imageUrl = blbThirdCompanion
-//     blbCompanionThree.setAttribute("src", imageUrl); // Set the image src
-// };
+const bloomburrowAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Ablb`);
+    let blbAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * blbAbilities.length);
+    
+    let randomAbility = blbAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        blbAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// const firstCompanionDmu = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Admu`);
-//     let dmuFirstCompanion = response.data.data[29].image_uris.art_crop;
-//     let imageUrl = dmuFirstCompanion
-//     dmuCompanionOne.setAttribute("src", imageUrl); // Set the image src
-// };
+const dominariaAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Admu`);
+    let dmuAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * dmuAbilities.length);
+    
+    let randomAbility = dmuAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        dmuAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// const secondCompanionDmu = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Admu`);
-//     let dmuSecondCompanion = response.data.data[29].image_uris.art_crop;
-//     let imageUrl = dmuSecondCompanion
-//     dmuCompanionTwo.setAttribute("src", imageUrl); // Set the image src
-// };
+const duskmournAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Adsk`);
+    let dskAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * dskAbilities.length);
+    
+    let randomAbility = dskAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        dskAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// const thirdCompanionDmu = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Admu`);
-//     let dmuThirdCompanion = response.data.data[29].image_uris.art_crop;
-//     let imageUrl = dmuThirdCompanion
-//     dmuCompanionThree.setAttribute("src", imageUrl); // Set the image src
-// };
+const murdersAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Amkm`);
+    let mkmAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * mkmAbilities.length);
+    
+    let randomAbility = mkmAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        mkmAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// ----------------------------------------ABILITIES---------------------------------------------------//
+const outlawsAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Aotj`);
+    let otjAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * otjAbility.length);
+    
+    let randomAbility = otjAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        otjAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-//-------------INITIAL PAGE LOAD IMAGE---------------------//
-// const companionLargeBlock = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Ablb`);
-//     let largeCompanionImage = response.data.data[83];
-//     let imageUrl = largeCompanionImage.image_uris?.art_crop;
-//     largeCompanion.setAttribute("src", imageUrl);
-// };
+// ------------------------------LARGE WINDOWS---------------------------------------------------//
 
-// // abilitySmallOne
-// const companionOneSmall = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Admu`);
-//     let smallCompanionImageOne = response.data.data[97];
-//     let imageUrl = smallCompanionImageOne.image_uris?.art_crop;
-//     companionSmallOne.setAttribute("src", imageUrl);
-// };
+const bloomburrowLAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Ablb`);
+    let blbLAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * blbLAbilities.length);
+    
+    let randomAbility = blbLAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        blbLAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// // abilitySmallTwo
-// const companionTwoSmall = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Adsk`);
-//     console.log(response.data.data)
-//     let smallCompanionImageTwo = response.data.data[100];
-//     let imageUrl = smallCompanionImageTwo.image_uris?.art_crop;
-//     companionSmallTwo.setAttribute("src", imageUrl);
-// };
+const dominariaLAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Admu`);
+    let dmuLAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * dmuLAbilities.length);
+    
+    let randomAbility = dmuLAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        dmuLAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// abilitySmallThree
-// const companionThreeSmall = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Amkm`);
-//     let smallCompanionImageThree = response.data.data[103];
-//     let imageUrl = smallCompanionImageThree.image_uris?.art_crop;
-//     companionSmallThree.setAttribute("src", imageUrl);
-// };
+const duskmournLAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Adsk`);
+    let dskLAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * dskLAbilities.length);
+    
+    let randomAbility = dskLAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        dskLAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// const companionThreeSmall = async () => {
-//     let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Aotj`);
-//     let smallCompanionImageThree = response.data.data[103];
-//     let imageUrl = smallCompanionImageThree.image_uris?.art_crop;
-//     companionSmallThree.setAttribute("src", imageUrl);
-// };
+const murdersLAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Amkm`);
+    let mkmLAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * mkmLAbilities.length);
+    
+    let randomAbility = mkmLAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        mkmLAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// // Function to change ability to the large image
-// function setLargeImage(source) {
-//     largeImage.src = source;
-// }
+const outlawsLAbility = async () => {
+    let response = await axios.get(`https://api.scryfall.com/cards/search?q=e%3Aotj`);
+    let otjLAbilities = response.data.data; 
+    
+    let randomIndex = Math.floor(Math.random() * otjLAbilities.length);
+    
+    let randomAbility = otjLAbilities[randomIndex];
+    
+    if (randomAbility.image_uris && randomAbility.image_uris.art_crop) {
+        let imageUrl = randomAbility.image_uris.art_crop;
+        
+        otjLAbilityOne.setAttribute("src", imageUrl);
+    }
+};
 
-// // Add event listeners to the small images
-// companionSmallOne.addEventListener("click", function () {
-//     setLargeImage(companionSmallOne.src);
-// });
 
-// companionSmallTwo.addEventListener("click", function () {
-//     setLargeImage(companionSmallTwo.src);
-// });
+bloomburrowCompanion();
+dominariaCompanion();
+duskmournCompanion();
+murdersCompanion();
+outlawsCompanion();
 
-// companionSmallThree.addEventListener("click", function () {
-//     setLargeImage(companionSmallThree.src);
-// });
+bloomburrowAbility();
+dominariaAbility();
+duskmournAbility();
+murdersAbility();
+outlawsAbility();
 
-// firstCompanionBlb();
-// secondCompanionBlb();
-// thirdCompanionBlb();
-
-// firstCompanionDmu();
-// secondCompanionDmu();
-// thirdCompanionDmu();
+bloomburrowLAbility();
+dominariaLAbility();
+duskmournLAbility();
+murdersLAbility();
+outlawsLAbility();
